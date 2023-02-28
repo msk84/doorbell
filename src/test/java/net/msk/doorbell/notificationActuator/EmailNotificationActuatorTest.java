d8ccdf0894e9e0ef4610f8c2775bce7fa07ebcfb
msk84 <florian@steinle.it>
2023-02-28 23:23:26 +0100

    * Get simple mail sender test working.

--- src/test/java/net/msk/doorbell/notificationActuator/EmailNotificationActuatorTest.java
+++ src/test/java/net/msk/doorbell/notificationActuator/EmailNotificationActuatorTest.java
@@ -1,31 +1,51 @@
 package net.msk.doorbell.notificationActuator;
 
+import net.msk.doorbell.DoorbellEvent;
 import net.msk.doorbell.service.DoorbellService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.util.ReflectionTestUtils.*;
 
+@ExtendWith(MockitoExtension.class)
 class EmailNotificationActuatorTest {
+
+    @Mock
+    private DoorbellService doorbellService;
+
+    @Mock
+    private MailSender mailSender;
 
     private EmailNotificationActuator emailNotificationActuator;
 
     @BeforeEach
     void setUp() {
-        final DoorbellService doorbellService = mock(DoorbellService.class);
-        final MailSender mailSender = mock(MailSender.class);
-        this.emailNotificationActuator = new EmailNotificationActuator(doorbellService, mailSender);
+        this.emailNotificationActuator = spy(new EmailNotificationActuator(this.doorbellService, this.mailSender));
+        setField(this.emailNotificationActuator, "mailSenderUser", "sender@test.com");
+        setField(this.emailNotificationActuator, "mailRecipients", "recipient@test.com");
     }
 
     @Test
-    void getType() {
+    void testGetType() {
         assertEquals(NotificationActuatorType.mail, this.emailNotificationActuator.getType());
     }
 
     @Test
-    void getActuatorDescription() {
+    void testGetActuatorDescription() {
         assertEquals("Email notification actuator", this.emailNotificationActuator.getActuatorDescription());
+    }
+
+    @Test
+    void testNotify() {
+        final DoorbellEvent doorbellTestEvent = new DoorbellEvent("event.qualifier", "This is a test event.");
+        this.emailNotificationActuator.notify(doorbellTestEvent);
+        verify(this.mailSender, times(1)).send(any(SimpleMailMessage.class));
     }
 }
